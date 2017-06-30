b111d582129ab9a3bd11b174eba5cc6996faaf77
dingzhenkai <13511577582@163.com>
2017-06-30 21:56:04 +0800

    floatingwindow

---
+++ AndroidStudioProjects/PositiveTime/app/src/main/java/com/sjtu/se2017/positivetime/ATapplicaion.java
@@ -0,0 +1,29 @@
+package com.sjtu.se2017.positivetime;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ATapplicaion extends Application {
+    private int AT;
+
+    public int getAT(){
+        return AT;
+    }
+
+    public void setAT(int AT){
+        this.AT = AT;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate(){
+        super.onTerminate();
+    }
+}
